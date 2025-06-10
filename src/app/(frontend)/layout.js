57a3ae19078c1e0adba942fc8433080954f99cb4
Matt <mfbrain@outlook.com>
2025-06-10 02:16:32 +0100

    use font Poppins

--- src/app/(frontend)/layout.js
+++ src/app/(frontend)/layout.js
@@ -1,4 +1,4 @@
-import { Open_Sans, Inter } from "next/font/google";
+import { Open_Sans, Poppins } from "next/font/google";
 import "./globals.css";
 
 const openSans = Open_Sans({
@@ -6,9 +6,11 @@ const openSans = Open_Sans({
   subsets: ["latin"],
 });
 
-const inter = Inter({
-  variable: "--font-inter",
+const poppins = Poppins({
+  weight: "400",
+  variable: "--font-poppins",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata = {
@@ -19,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body id="bodyElement" className={`${inter.variable}  antialiased`}>
+      <body id="bodyElement" className={`font-primary antialiased`}>
         {/* screen size indicator - for dev */}
         {/* <div className="fixed top-7 md:top-27 md:left-12 left-0 bg-red md:bg-blue z-100 font-bold w-20 h-20 lg:text-orange-500 text-amber-400 sm:text-red-500 md:text-blue-500 xl:text-green-500 after:content-['mobile(<640px)'] sm:after:content-['sm(>640px)'] md:after:content-['md(>768px)'] lg:after:content-['lg(>1024px)'] xl:after:content-['xl(>1280px)']">
           <p>Screen</p>
