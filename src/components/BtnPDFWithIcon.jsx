478afe7b05d34f2340b524f76ac86b940fc6d159
Matt <mfbrain@outlook.com>
2025-06-10 04:03:13 +0100

    add pdf file and amend href of download btns

--- src/components/BtnPDFWithIcon.jsx
+++ src/components/BtnPDFWithIcon.jsx
@@ -3,11 +3,11 @@ import IconDownload from "../../public/icon-download.svg";
 
 const BtnPDF = () => {
   return (
-    <a className="inline-block" href="/remitech-prompting-guide.pdf" download>
-      <div className="py-3 pt-[14px] px-6 bg-white text-remitech-turquoise hover:text-remitech-purple transition font-normal text-base cursor-pointer rounded-lg flex justify-between">
-        <span className="pr-6">Download</span>
+    <a className="inline-block" href="/prompting-guide-101.pdf" download>
+      <div className="py-5 pt-[20px] px-11 bg-white text-remitech-turquoise hover:text-remitech-purple transition font-normal text-base cursor-pointer rounded-lg flex justify-between group">
+        <span className="pr-6 font-bold">Download</span>
 
-        <IconDownload className="w-6" />
+        <IconDownload className="w-6 fill-[#2BB9D6] group-hover:fill-remitech-purple transition" />
       </div>
     </a>
   );
