0040ecd633f3eeb1b180b23218bd7a04ca360258
Richard Brain <richard.brain@hotmail.co.uk>
2025-06-16 17:28:27 +0100

    update event copy

--- src/components/Event2.jsx
+++ src/components/Event2.jsx
@@ -10,10 +10,7 @@ const eventData = {};
 eventData.eventNum = 2;
 eventData.eventText = [];
 eventData.eventText.push(
-  `Join Us for an evening of great food, drinks, conversation, and an informal Q&A with Arek Dreyer, renowned Apple expert and trainer, on Thursday, 26th June from 6:00pm at a Kings Cross rooftop.`
-);
-eventData.eventText.push(
-  `Arek will be sharing his thoughts on what’s new and noteworthy for MacAdmins following WWDC25 — plus answering your questions live on the night.`
+  `Join Us for an evening of great food, drinks, conversation, and informal Q&A on Thursday, 26th June from 6:00pm at a Kings Cross rooftop.`
 );
 eventData.eventText.push(
   `Spaces are limited, so please register below to secure your place.`
@@ -22,7 +19,7 @@ eventData.eventText.push(
 eventData.imageSrc = "/event2-image.png";
 eventData.imageAlt = "London Skyline";
 eventData.eventTitle = "Rooftop BBQ";
-eventData.eventSubTitle = `An Evening with <span style="white-space: nowrap;">Arek Dreyer</span>`;
+eventData.eventSubTitle = `An Evening with Remitech`;
 
 const Event2 = () => {
   return <EventTemplate eventData={eventData} textPos="left" />;
