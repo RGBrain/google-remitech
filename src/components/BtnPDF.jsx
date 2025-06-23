eb6ff20fbb1fd27f3dffcdadaca19c8649b49a47
Matt <mfbrain@outlook.com>
2025-06-23 09:15:26 +0100

    make btns 'Download Prompting Guide 101' scroll to GateForm

--- src/components/BtnPDF.jsx
+++ src/components/BtnPDF.jsx
@@ -4,22 +4,22 @@ import React from "react";
 import { useScrollPosition } from "@thibault.sh/hooks/useScrollPosition";
 import { track } from "@vercel/analytics";
 
+function scrollToElement(id) {
+  const element = document.getElementById(id);
+  if (element) {
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
 const BtnPDF = () => {
   const { y } = useScrollPosition();
   return (
-    <a
-      href="/Prompting-Guide-101-Remitech.pdf"
-      download
-      onClick={() => {
-        track("DownloadPdf");
-      }}
+    <button
+      onClick={() => scrollToElement("downloadPDF")}
+      className={`mt-3 p-3 px-5 ${y > 0 ? `text-remitech-purple hover:text-remitech-turquoise from-white to-white` : `from-remitech-purple to-remitech-turquoise hover:to-remitech-purple hover:from-remitech-purple text-white`} transform cursor-pointer rounded-md bg-gradient-to-tr text-base font-bold transition duration-200 ease-in hover:scale-102`}
     >
-      <button
-        className={`mt-3 p-3 px-5 ${y > 0 ? `text-remitech-purple hover:text-remitech-turquoise from-white to-white` : `from-remitech-purple to-remitech-turquoise hover:to-remitech-purple hover:from-remitech-purple text-white`} transform cursor-pointer rounded-md bg-gradient-to-tr text-base font-bold transition duration-200 ease-in hover:scale-102`}
-      >
-        Download Prompting Guide 101
-      </button>
-    </a>
+      Download Prompting Guide 101
+    </button>
   );
 };
 
--- src/components/BtnPDFGate.jsx
+++ src/components/BtnPDFGate.jsx
@@ -3,19 +3,21 @@
 import React from "react";
 import { track } from "@vercel/analytics";
 
+function scrollToElement(id) {
+  const element = document.getElementById(id);
+  if (element) {
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
 const BtnPDFGate = () => {
   return (
-    <a
-      href="/Prompting-Guide-101-Remitech.pdf"
-      download
-      onClick={() => {
-        track("DownloadPdf");
-      }}
+    <button
+      onClick={() => scrollToElement("downloadPDF")}
+      className="text-remitech-purple hover:text-remitech-turquoise mt-3 transform cursor-pointer rounded-md bg-gradient-to-tr from-white to-white p-3 px-5 text-base font-bold transition duration-200 ease-in hover:scale-102"
     >
-      <button className="text-remitech-purple hover:text-remitech-turquoise mt-3 transform cursor-pointer rounded-md bg-gradient-to-tr from-white to-white p-3 px-5 text-base font-bold transition duration-200 ease-in hover:scale-102">
-        Download Prompting Guide 101
-      </button>
-    </a>
+      Download Prompting Guide 101
+    </button>
   );
 };
 
