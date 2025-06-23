69d1a2fd7e000fb54a52d93af0dd28ccb338f9a7
Matt <mfbrain@outlook.com>
2025-06-23 10:01:56 +0100

    adjust scrollTo position on button clicks

--- src/components/BtnPDF.jsx
+++ src/components/BtnPDF.jsx
@@ -15,7 +15,7 @@ const BtnPDF = () => {
   const { y } = useScrollPosition();
   return (
     <button
-      onClick={() => scrollToElement("downloadPDF")}
+      onClick={() => scrollToElement("s2")}
       className={`mt-3 p-3 px-5 ${y > 0 ? `text-remitech-purple hover:text-remitech-turquoise from-white to-white` : `from-remitech-purple to-remitech-turquoise hover:to-remitech-purple hover:from-remitech-purple text-white`} transform cursor-pointer rounded-md bg-gradient-to-tr text-base font-bold transition duration-200 ease-in hover:scale-102`}
     >
       Download Prompting Guide 101
